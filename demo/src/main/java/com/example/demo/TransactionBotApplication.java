cccbc072a68dbd577b5331f6e04929f8de421f2d
ctecte <legendarycte@gmail.com>
2025-10-16 05:52:11 +0000

    added the spring boot init with spring rest dependencies and JDBC

---
+++ demo/src/main/java/com/example/demo/TransactionBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionBotApplication.class, args);
+	}
+
+}
